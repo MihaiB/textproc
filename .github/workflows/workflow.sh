569a17efcd2e90d9641686c1a81c29a90cf9fbc2
Mihai Borobocea <mihborobo@gmail.com>
2020-07-06 00:10:26 +0300

    show the gofmt diff in CI (#8)
    * intentionally misformat a Go file
    
    * show the gofmt diff in CI
    
    * ci: replace incorrect exclamation mark (!) use
    
    * ci: remove redundant file name from the gofmt diff output
    
    * go fmt

--- .github/workflows/workflow.sh
+++ .github/workflows/workflow.sh
@@ -6,8 +6,15 @@ trap "echo >&2 script '${BASH_SOURCE[0]}' failed" ERR
 SCRIPT=`readlink -e "${BASH_SOURCE[0]}"`
 SCRIPT_DIR=`dirname "$SCRIPT"`
 cd "$SCRIPT_DIR"/../..
+unset SCRIPT SCRIPT_DIR
 
-! gofmt -l -s . | grep -q ''
+GOFMT_OUTPUT=`gofmt -l -s .`
+if [ -n "$GOFMT_OUTPUT" ]; then
+	gofmt -d -s .
+	false
+fi
+unset GOFMT_OUTPUT
+
 go vet ./...
 go test ./...
 go install ./...
